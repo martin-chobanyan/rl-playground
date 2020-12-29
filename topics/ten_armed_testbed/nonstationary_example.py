ea6570048d9331bda3178c28fa0952f12e55c58b
unknown <chobanyan.martin@gmail.com>
2020-12-28 19:27:42 -0500

    added nonstationarity

--- topics/ten_armed_testbed/nonstationary_example.py
+++ topics/ten_armed_testbed/nonstationary_example.py
@@ -5,7 +5,7 @@ import numpy as np
 from tqdm import tqdm
 
 
-def run_k_arm_bandit(n_steps, n_arms, eps, alpha=None):
+def run_nonstationary_k_arm_bandit(n_steps, n_arms, eps, alpha=None):
     """Run the K-arm bandit problem
 
     Parameters
@@ -29,20 +29,21 @@ def run_k_arm_bandit(n_steps, n_arms, eps, alpha=None):
     # sample the true q* value per action from a unit Gaussian
     true_action_vals = np.random.randn(n_arms)
 
-    # sample the rewards per action for each step
-    # resulting array will have shape (n_steps, n_arms)
-    sample_rewards = np.stack([np.random.normal(q_a, 1, n_steps) for q_a in true_action_vals]).T
-
     actions_taken = []
     rewards_received = []
-    for rewards in sample_rewards:
+    for _ in range(n_steps):
         # exploration: choose random action with probability `eps`
         if np.random.rand() < eps:
             action = np.random.randint(0, n_arms)
         # exploitation: select the action with the highest current estimated value
         else:
             action = qn_per_action.argmax()
-        curr_reward = rewards[action]
+
+        # add some noise to the true action-values
+        true_action_vals += np.random.normal(0, 0.01, n_arms)
+
+        # sample the reward from this selected action
+        curr_reward = np.random.normal(true_action_vals[action], 1)
 
         # update the action count
         action_counts[action] += 1
@@ -61,12 +62,12 @@ def run_k_arm_bandit(n_steps, n_arms, eps, alpha=None):
     return actions_taken, rewards_received, true_action_vals
 
 
-def run_ten_armed_testbed(n_runs, n_steps, eps):
+def run_ten_armed_testbed(n_runs, n_steps, eps, alpha=None):
     actions = []
     rewards = []
     true_action_values = []
     for _ in tqdm(range(n_runs), desc=f'Running for epsilon = {eps}'):
-        a, r, tav = run_k_arm_bandit(n_steps=n_steps, n_arms=10, eps=eps)
+        a, r, tav = run_nonstationary_k_arm_bandit(n_steps=n_steps, n_arms=10, eps=eps, alpha=alpha)
         actions.append(a)
         rewards.append(r)
         true_action_values.append(tav)
@@ -76,36 +77,30 @@ def run_ten_armed_testbed(n_runs, n_steps, eps):
     return actions, rewards, true_action_values
 
 
-def main(n_runs=2000, n_steps=1000):
-    # run the experiments
-    actions_0, rewards_0, true_values_0 = run_ten_armed_testbed(n_runs, n_steps, 0)
-    actions_1p, rewards_1p, true_values_1p = run_ten_armed_testbed(n_runs, n_steps, 0.01)
-    actions_10p, rewards_10p, true_values_10p = run_ten_armed_testbed(n_runs, n_steps, 0.1)
+def main(n_runs=2000, n_steps=10000):
+    # run the experiments (stationary vs nonstationary)
+    actions_s, rewards_s, true_values_s = run_ten_armed_testbed(n_runs, n_steps, eps=0.1, alpha=None)
+    actions_n, rewards_n, true_values_n = run_ten_armed_testbed(n_runs, n_steps, eps=0.1, alpha=0.1)
 
     fig, (ax1, ax2) = plt.subplots(2, 1)
 
     # first plot: average reward over time
-    mean_reward_per_step_0 = rewards_0.mean(axis=0)
-    mean_reward_per_step_1p = rewards_1p.mean(axis=0)
-    mean_reward_per_step_10p = rewards_10p.mean(axis=0)
-    ax1.plot(range(n_steps), mean_reward_per_step_0, color='green', label='Eps=0 (greedy)')
-    ax1.plot(range(n_steps), mean_reward_per_step_1p, color='red', label='Eps=0.01')
-    ax1.plot(range(n_steps), mean_reward_per_step_10p, color='blue', label='Eps=0.1')
+    mean_reward_per_step_s = rewards_s.mean(axis=0)
+    mean_reward_per_step_n = rewards_n.mean(axis=0)
+    ax1.plot(range(n_steps), mean_reward_per_step_s, label='Stationary (a=1/n)')
+    ax1.plot(range(n_steps), mean_reward_per_step_n, label='Nonstationary (a=0.1)')
     ax1.set_xlabel('Steps')
     ax1.set_ylabel('Average reward')
 
     # second plot: optimal actions per step
-    optimal_actions_0 = true_values_0.argmax(axis=1).reshape(-1, 1)
-    optimal_actions_1p = true_values_1p.argmax(axis=1).reshape(-1, 1)
-    optimal_actions_10p = true_values_10p.argmax(axis=1).reshape(-1, 1)
+    optimal_actions_s = true_values_s.argmax(axis=1).reshape(-1, 1)
+    optimal_actions_n = true_values_n.argmax(axis=1).reshape(-1, 1)
 
-    percent_optimal_actions_0 = (actions_0 == optimal_actions_0).sum(axis=0) / actions_0.shape[0]
-    percent_optimal_actions_1p = (actions_1p == optimal_actions_1p).sum(axis=0) / actions_1p.shape[0]
-    percent_optimal_actions_10p = (actions_10p == optimal_actions_10p).sum(axis=0) / actions_10p.shape[0]
+    percent_optimal_actions_0 = (actions_s == optimal_actions_s).sum(axis=0) / actions_s.shape[0]
+    percent_optimal_actions_1p = (actions_n == optimal_actions_n).sum(axis=0) / actions_n.shape[0]
 
-    ax2.plot(range(n_steps), percent_optimal_actions_0, color='green', label='Eps=0 (greedy)')
-    ax2.plot(range(n_steps), percent_optimal_actions_1p, color='red', label='Eps=0.01')
-    ax2.plot(range(n_steps), percent_optimal_actions_10p, color='blue', label='Eps=0.1')
+    ax2.plot(range(n_steps), percent_optimal_actions_0, label='Sample-average (a=1/n)')
+    ax2.plot(range(n_steps), percent_optimal_actions_1p, label='Constant step size (a=0.1)')
     ax2.set_xlabel('Steps')
     ax2.set_ylabel('% Optimal action')
 
