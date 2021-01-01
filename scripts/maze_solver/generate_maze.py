81af5a29a3a26170a7f7da909451cd797729e015
unknown <chobanyan.martin@gmail.com>
2020-12-31 20:46:46 -0500

    simplified maze generator

--- scripts/maze_solver/generate_maze.py
+++ scripts/maze_solver/generate_maze.py
@@ -1,241 +1,149 @@
-## Imports
 import random
 
 import matplotlib.pyplot as plt
 import numpy as np
 
-## Functions
 
-# Find number of surrounding cells
-def surroundingCells(rand_wall):
-    s_cells = 0
-    if (maze[rand_wall[0] - 1][rand_wall[1]] == 0):
-        s_cells += 1
-    if (maze[rand_wall[0] + 1][rand_wall[1]] == 0):
-        s_cells += 1
-    if (maze[rand_wall[0]][rand_wall[1] - 1] == 0):
-        s_cells += 1
-    if (maze[rand_wall[0]][rand_wall[1] + 1] == 0):
-        s_cells += 1
-
-    return s_cells
+def surrounding_cells(rand_wall, val=0):
+    count = 0
+    for y_diff, x_diff in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        if maze[rand_wall[0] + y_diff, rand_wall[1] + x_diff] == val:
+            count += 1
+    return count
 
 
-## Main code
-# Init variables
+def leftmost_cell(maze, walls, x, y, wall_val=1):
+    if x != 0:
+        if maze[y, x - 1] != 0:
+            maze[y, x - 1] = wall_val
+        if [y, x - 1] not in walls:
+            walls.append([y, x - 1])
+
+
+def rightmost_cell(maze, walls, x, y, wall_val=1):
+    _, grid_width = maze.shape
+    if x != grid_width - 1:
+        if maze[y, x + 1] != 0:
+            maze[y, x + 1] = wall_val
+        if [y, x + 1] not in walls:
+            walls.append([y, x + 1])
+
+
+def uppermost_cell(maze, walls, x, y, wall_val=1):
+    if y != 0:
+        if maze[y - 1, x] != 0:
+            maze[y - 1, x] = wall_val
+        if [y - 1, x] not in walls:
+            walls.append([y - 1, x])
+
+
+def bottommost_cell(maze, walls, x, y, wall_val=1):
+    if y != GRID_HEIGHT - 1:
+        if maze[y + 1, x] != 0:
+            maze[y + 1, x] = wall_val
+        if [y + 1, x] not in walls:
+            walls.append([y + 1, x])
+
+
 wall = 1
 cell = 0
 unvisited = -1
-GRID_HEIGHT = 100
-GRID_WIDTH = 100
-maze = []
 
-
-
-# Denote all cells as unvisited
-for i in range(0, GRID_HEIGHT):
-    line = []
-    for j in range(0, GRID_WIDTH):
-        line.append(unvisited)
-    maze.append(line)
+GRID_HEIGHT = 20
+GRID_WIDTH = 40
+maze = np.full((GRID_HEIGHT, GRID_WIDTH), -1)
 
 # Randomize starting point and set it a cell
-starting_height = int(random.random() * GRID_HEIGHT)
-starting_width = int(random.random() * GRID_WIDTH)
-if (starting_height == 0):
-    starting_height += 1
-if (starting_height == GRID_HEIGHT - 1):
-    starting_height -= 1
-if (starting_width == 0):
-    starting_width += 1
-if (starting_width == GRID_WIDTH - 1):
-    starting_width -= 1
+start_y = np.random.randint(1, GRID_HEIGHT - 1)
+start_x = np.random.randint(1, GRID_WIDTH - 1)
 
 # Mark it as cell and add surrounding walls to the list
-maze[starting_height][starting_width] = cell
-walls = []
-walls.append([starting_height - 1, starting_width])
-walls.append([starting_height, starting_width - 1])
-walls.append([starting_height, starting_width + 1])
-walls.append([starting_height + 1, starting_width])
+maze[start_y, start_x] = cell
+walls = [
+    [start_y - 1, start_x],
+    [start_y, start_x - 1],
+    [start_y, start_x + 1],
+    [start_y + 1, start_x]
+]
+for y_wall, x_wall in walls:
+    maze[y_wall, x_wall] = wall
 
-# Denote walls in maze
-maze[starting_height - 1][starting_width] = 1
-maze[starting_height][starting_width - 1] = 1
-maze[starting_height][starting_width + 1] = 1
-maze[starting_height + 1][starting_width] = 1
-
-while (walls):
-    # Pick a random wall
-    rand_wall = walls[int(random.random() * len(walls)) - 1]
+while walls:
+    random_wall = random.choice(walls)
+    y_wall, x_wall = random_wall
 
     # Check if it is a left wall
-    if (rand_wall[1] != 0):
-        if (maze[rand_wall[0]][rand_wall[1] - 1] == -1 and maze[rand_wall[0]][rand_wall[1] + 1] == 0):
-            # Find the number of surrounding cells
-            s_cells = surroundingCells(rand_wall)
-
-            if (s_cells < 2):
+    if x_wall != 0:
+        if maze[y_wall, x_wall - 1] == -1 and maze[y_wall, x_wall + 1] == 0:
+            if surrounding_cells(random_wall) < 2:
                 # Denote the new path
-                maze[rand_wall[0]][rand_wall[1]] = 0
+                maze[y_wall, x_wall] = 0
 
                 # Mark the new walls
-                # Upper cell
-                if (rand_wall[0] != 0):
-                    if (maze[rand_wall[0] - 1][rand_wall[1]] != 0):
-                        maze[rand_wall[0] - 1][rand_wall[1]] = 1
-                    if ([rand_wall[0] - 1, rand_wall[1]] not in walls):
-                        walls.append([rand_wall[0] - 1, rand_wall[1]])
+                uppermost_cell(maze, walls, x_wall, y_wall)
+                bottommost_cell(maze, walls, x_wall, y_wall)
+                leftmost_cell(maze, walls, x_wall, y_wall)
 
-                # Bottom cell
-                if (rand_wall[0] != GRID_HEIGHT - 1):
-                    if (maze[rand_wall[0] + 1][rand_wall[1]] != 0):
-                        maze[rand_wall[0] + 1][rand_wall[1]] = 1
-                    if ([rand_wall[0] + 1, rand_wall[1]] not in walls):
-                        walls.append([rand_wall[0] + 1, rand_wall[1]])
-
-                # Leftmost cell
-                if (rand_wall[1] != 0):
-                    if (maze[rand_wall[0]][rand_wall[1] - 1] != 0):
-                        maze[rand_wall[0]][rand_wall[1] - 1] = 1
-                    if ([rand_wall[0], rand_wall[1] - 1] not in walls):
-                        walls.append([rand_wall[0], rand_wall[1] - 1])
-
-            # Delete wall
-            for wall in walls:
-                if (wall[0] == rand_wall[0] and wall[1] == rand_wall[1]):
-                    walls.remove(wall)
-
+            walls.remove(random_wall)
             continue
 
     # Check if it is an upper wall
-    if (rand_wall[0] != 0):
-        if (maze[rand_wall[0] - 1][rand_wall[1]] == -1 and maze[rand_wall[0] + 1][rand_wall[1]] == 0):
-
-            s_cells = surroundingCells(rand_wall)
-            if (s_cells < 2):
+    if y_wall != 0:
+        if maze[y_wall - 1, x_wall] == -1 and maze[y_wall + 1, x_wall] == 0:
+            if surrounding_cells(random_wall) < 2:
                 # Denote the new path
-                maze[rand_wall[0]][rand_wall[1]] = 0
+                maze[y_wall, x_wall] = 0
 
                 # Mark the new walls
-                # Upper cell
-                if (rand_wall[0] != 0):
-                    if (maze[rand_wall[0] - 1][rand_wall[1]] != 0):
-                        maze[rand_wall[0] - 1][rand_wall[1]] = 1
-                    if ([rand_wall[0] - 1, rand_wall[1]] not in walls):
-                        walls.append([rand_wall[0] - 1, rand_wall[1]])
+                uppermost_cell(maze, walls, x_wall, y_wall)
+                leftmost_cell(maze, walls, x_wall, y_wall)
+                rightmost_cell(maze, walls, x_wall, y_wall)
 
-                # Leftmost cell
-                if (rand_wall[1] != 0):
-                    if (maze[rand_wall[0]][rand_wall[1] - 1] != 0):
-                        maze[rand_wall[0]][rand_wall[1] - 1] = 1
-                    if ([rand_wall[0], rand_wall[1] - 1] not in walls):
-                        walls.append([rand_wall[0], rand_wall[1] - 1])
-
-                # Rightmost cell
-                if (rand_wall[1] != GRID_WIDTH - 1):
-                    if (maze[rand_wall[0]][rand_wall[1] + 1] != 0):
-                        maze[rand_wall[0]][rand_wall[1] + 1] = 1
-                    if ([rand_wall[0], rand_wall[1] + 1] not in walls):
-                        walls.append([rand_wall[0], rand_wall[1] + 1])
-
-            # Delete wall
-            for wall in walls:
-                if (wall[0] == rand_wall[0] and wall[1] == rand_wall[1]):
-                    walls.remove(wall)
-
+            walls.remove(random_wall)
             continue
 
     # Check the bottom wall
-    if (rand_wall[0] != GRID_HEIGHT - 1):
-        if (maze[rand_wall[0] + 1][rand_wall[1]] == -1 and maze[rand_wall[0] - 1][rand_wall[1]] == 0):
-
-            s_cells = surroundingCells(rand_wall)
-            if (s_cells < 2):
+    if y_wall != GRID_HEIGHT - 1:
+        if maze[y_wall + 1, x_wall] == -1 and maze[y_wall - 1, x_wall] == 0:
+            if surrounding_cells(random_wall) < 2:
                 # Denote the new path
-                maze[rand_wall[0]][rand_wall[1]] = 0
+                maze[y_wall, x_wall] = 0
 
                 # Mark the new walls
-                if (rand_wall[0] != GRID_HEIGHT - 1):
-                    if (maze[rand_wall[0] + 1][rand_wall[1]] != 0):
-                        maze[rand_wall[0] + 1][rand_wall[1]] = 1
-                    if ([rand_wall[0] + 1, rand_wall[1]] not in walls):
-                        walls.append([rand_wall[0] + 1, rand_wall[1]])
-                if (rand_wall[1] != 0):
-                    if (maze[rand_wall[0]][rand_wall[1] - 1] != 0):
-                        maze[rand_wall[0]][rand_wall[1] - 1] = 1
-                    if ([rand_wall[0], rand_wall[1] - 1] not in walls):
-                        walls.append([rand_wall[0], rand_wall[1] - 1])
-                if (rand_wall[1] != GRID_WIDTH - 1):
-                    if (maze[rand_wall[0]][rand_wall[1] + 1] != 0):
-                        maze[rand_wall[0]][rand_wall[1] + 1] = 1
-                    if ([rand_wall[0], rand_wall[1] + 1] not in walls):
-                        walls.append([rand_wall[0], rand_wall[1] + 1])
+                leftmost_cell(maze, walls, x_wall, y_wall)
+                rightmost_cell(maze, walls, x_wall, y_wall)
+                bottommost_cell(maze, walls, x_wall, y_wall)
 
-            # Delete wall
-            for wall in walls:
-                if (wall[0] == rand_wall[0] and wall[1] == rand_wall[1]):
-                    walls.remove(wall)
-
+            walls.remove(random_wall)
             continue
 
     # Check the right wall
-    if (rand_wall[1] != GRID_WIDTH - 1):
-        if (maze[rand_wall[0]][rand_wall[1] + 1] == -1 and maze[rand_wall[0]][rand_wall[1] - 1] == 0):
-
-            s_cells = surroundingCells(rand_wall)
-            if (s_cells < 2):
+    if x_wall != GRID_WIDTH - 1:
+        if maze[y_wall, x_wall + 1] == -1 and maze[y_wall, x_wall - 1] == 0:
+            if surrounding_cells(random_wall) < 2:
                 # Denote the new path
-                maze[rand_wall[0]][rand_wall[1]] = 0
+                maze[y_wall, x_wall] = 0
 
                 # Mark the new walls
-                if (rand_wall[1] != GRID_WIDTH - 1):
-                    if (maze[rand_wall[0]][rand_wall[1] + 1] != 0):
-                        maze[rand_wall[0]][rand_wall[1] + 1] = 1
-                    if ([rand_wall[0], rand_wall[1] + 1] not in walls):
-                        walls.append([rand_wall[0], rand_wall[1] + 1])
-                if (rand_wall[0] != GRID_HEIGHT - 1):
-                    if (maze[rand_wall[0] + 1][rand_wall[1]] != 0):
-                        maze[rand_wall[0] + 1][rand_wall[1]] = 1
-                    if ([rand_wall[0] + 1, rand_wall[1]] not in walls):
-                        walls.append([rand_wall[0] + 1, rand_wall[1]])
-                if (rand_wall[0] != 0):
-                    if (maze[rand_wall[0] - 1][rand_wall[1]] != 0):
-                        maze[rand_wall[0] - 1][rand_wall[1]] = 1
-                    if ([rand_wall[0] - 1, rand_wall[1]] not in walls):
-                        walls.append([rand_wall[0] - 1, rand_wall[1]])
+                rightmost_cell(maze, walls, x_wall, y_wall)
+                bottommost_cell(maze, walls, x_wall, y_wall)
+                uppermost_cell(maze, walls, x_wall, y_wall)
 
-            # Delete wall
-            for wall in walls:
-                if (wall[0] == rand_wall[0] and wall[1] == rand_wall[1]):
-                    walls.remove(wall)
-
+            walls.remove(random_wall)
             continue
 
-    # Delete the wall from the list anyway
-    for wall in walls:
-        if (wall[0] == rand_wall[0] and wall[1] == rand_wall[1]):
-            walls.remove(wall)
+    walls.remove(random_wall)
 
 # Mark the remaining unvisited cells as walls
-for i in range(0, GRID_HEIGHT):
-    for j in range(0, GRID_WIDTH):
-        if (maze[i][j] == -1):
-            maze[i][j] = 1
+maze[maze == -1] = 1
 
 # Set entrance and exit
-for i in range(0, GRID_WIDTH):
-    if (maze[1][i] == 0):
-        maze[0][i] = 0
-        break
+x_entrance = np.where(maze[1, :] == 0)[0][0]  # x-coord of the first open cell in the second row
+x_exit = np.where(maze[-2, :] == 0)[0][-1]    # x-coord of the first open cell in the penultimate row
+maze[0, x_entrance] = 0
+maze[-1, x_exit] = 0
 
-for i in range(GRID_WIDTH - 1, 0, -1):
-    if (maze[GRID_HEIGHT - 2][i] == 0):
-        maze[GRID_HEIGHT - 1][i] = 0
-        break
-
-maze = np.array(maze)
-
-# plt.imshow(maze, cmap='gray')
-plt.imshow(maze)
+maze = np.where(maze == 0, 1, 0)
+plt.imshow(maze, cmap='gray')
+plt.axis('off')
 plt.show()
